acce94c2e588a14807b5df21c760a7410698e312
Gonzalo Avila Alterach <gonzaloavilaalterach@gmail.com>
2023-11-15 22:07:02 -0300

    Fixed menu commit

--- app.c
+++ app.c
@@ -3,56 +3,55 @@
 #include <printer.h>
 #include <aclconio.h>
 #include <aclstr.h>
+#include <stdlib.h>
 
-char greeting2[] = "Subir y bajar con las flechas, elegir con el boton verde";
+char greeting1[] = "Subir y bajar con las flechas, elegir con el boton verde";
 
-char greeting1[] = " POS Cyberciruja 2023";
-char ack[] = "         by gzalo.com";
+char greeting2[] = " POS Cyberciruja 2023";
+char greeting3[] = "\x14\x15 \x16\x17    by gzalo.com";
 
-// TODO:
-// Add menu and random mode
-
-char artNames[40][18] = {
-    "Cybercirujas",
-    "Flashparty",
-    "Linux",
-    "Raspberry Pi",
-    "Exceso de Windows",
-    "Boleto de bondi",
-    "Cara Soldan",
-    "Replay",
-    "Endif",
-    "Poder Alien",
-    "Floppy disk",
-    "Right to Repair",
-    "Caras del Doom",
-    "Linux inside",
-    "Circuito",
-    "Xonotic",
-    "Carpincho",
-    "Joy Division",
-    "Daft Punk",
-    "Mate",
-    "Don Epi",
-    "Copas",
-    "Posadas Vice",
-    "General",
-    "Fantasma",
-    "Mandelbrot",
-    "Sierpinski",
-    "Lines",
-    "Gatos",
-    "Contiene Linux",
-    "Mapa cyberciruja",
-    "Commodore 64",
-    "Z80 inside",
-    "Partes de PC",
-    "Isaac",
-    "Super Hexagon",
-    "Tetris",
-    "Half-Life",
-    "Portal gun",
-    "Conectar Igualdad"
+char artNames[41][25] = {
+    "00. Al azar",
+    "01. Cybercirujas",
+    "02. Flashparty",
+    "03. Linux",
+    "04. Raspberry Pi",
+    "05. Exceso de Windows",
+    "06. Boleto de bondi",
+    "07. Cara Soldan",
+    "08. Replay",
+    "09. Endif",
+    "10. Poder Alien",
+    "11. Floppy disk",
+    "12. Right to Repair",
+    "13. Caras del Doom",
+    "14. Linux inside",
+    "15. Circuito",
+    "16. Xonotic",
+    "17. Carpincho",
+    "18. Joy Division",
+    "19. Daft Punk",
+    "20. Mate",
+    "21. Don Epi",
+    "22. Copas",
+    "23. Posadas Vice",
+    "24. General",
+    "25. Fantasma",
+    "26. Mandelbrot",
+    "27. Sierpinski",
+    "28. Lines",
+    "29. Gatos",
+    "30. Contiene Linux",
+    "31. Mapa cyberciruja",
+    "32. Commodore 64",
+    "33. Z80 inside",
+    "34. Partes de PC",
+    "35. Isaac",
+    "36. Super Hexagon",
+    "37. Tetris",
+    "38. Half-Life",
+    "39. Portal gun",
+    "40. Conectar Igualdad"
 };
 
 char filenames[40][16] = {
@@ -98,72 +97,121 @@ char filenames[40][16] = {
     "I:1/39.bmp",    
 };
 
+// 000 EMPTY 0-15
+// 016 BAT-BAT PALO-PALO FLECHA-ARR FLECHA-ABAJO DERGRUESA IZQGRUESA ArrDer Arr Abaj Enter Der Izq
+// 032 Space !(33) " # ...
+// 048 012345 ...
+// 064 @ABC...
+// 080 PQR...
+// 096 `abc
+// 112 prq...~ BLOQUENEGRO(127)
+// 128 € PuntaBat bat chica.... Tick Cruz BloqueNegro BloqueNegro BloqueNegro
+// 144 BLOQUES NEGROS
+// 160 BLOQUENEGRO ¡ Cent Libra XRara Yen | Simoleon ¨ Copy ª << ¬ Negro ? -
+// 176 º +- 2 3 ´ micro parraf · , 1 º >> 1/4 1/2 3/4 ¿
+// 192 à á â ä a· AE Ç è é ê ë ì í î ï
+// 208 Dmedio Ñ ò ó ô õ ö x O ù ú û ü Ý D Beta
+// 224 a· a· aº ã ä a$ ae ç è é ê ë ì í î ï 
+// 240 dx ñ ò ó ô õ ö div ss ù ù û ü ý d ÿ
 void fontLoop() {
-    uint8_t i = 0;
+    unsigned char i = 0;
     while(1){
         clrscr();
-        uint8_t buffer[17]; 
-        for (uint8_t j = 0; j < 16; j++) {
+        unsigned char buffer[17]; 
+        for (unsigned char j = 0; j < 16; j++) {
             buffer[j] = i + j;
         }
         buffer[16] = '\0';
 
-        write_at(buffer, strlen(buffer), 1, 1);
+        write_at((char*)buffer, strlen((char*)buffer), 1, 1);
         SVC_WAIT(1000);
-        i+= 256;
+        i+= 16;
     }
 }
 
 char padding[] = "I:1/padding.bmp";
 
-char szKeyMap[MAX_ALPNUM_KEYS][CHAR_PER_KEY]={
-           "0- +Q",   "1Z.AB",   "2CDEF",  "3GHIJ",  "4KLMN",  "5OPRS",
-           "6TUVW",   "7XY*,",   "8'\"#=",  "9:$~^",  "{}[]\\","&%()!"};
-
 int main (void){
     int console = open(DEV_CONSOLE, 0);
     int hPrinter = open(DEV_COM4, 0);
     clrscr();
+    set_cursor(0);
 
-    fontLoop();
+    int cursorPos = 0;
+    int winOffset = 0;
+    const int WIN_HEIGHT = 4;
+    const int TOTAL_ELEMENTS = 41;
+    const int TOTAL_LOGOS = 40;
 
     while(1) {
         set_backlight(1);
-        write_at(greeting2, strlen(greeting2), 1, 1);  
 
-        write_at(greeting1, strlen(greeting1), 1, 7);  
-        write_at(ack, strlen(ack), 1, 8);  
-        normal_tone();
+        write_at(greeting1, strlen(greeting1), 1, 1);  
+        write_at(greeting2, strlen(greeting2), 1, 7);  
+        write_at(greeting3, strlen(greeting3), 1, 8); 
 
-        signed char buff[21];
-        int artIndex=0; 
-        memset(buff, 0x0, sizeof(buff));
-        set_cursor(0);
-
-        gotoxy(1,2);
-
-        int ret = getkbd_entry(0, "", buff, (unsigned int) 100000,
-					    (unsigned int)NUMERIC, (char *)szKeyMap, sizeof(szKeyMap), 2, 1);
-
-        if(ret <= 0) continue;
-
-        artIndex = strn2int((char*)buff,2) - 1;
-
-        if(artIndex > 39 || artIndex < 0) {
-            continue;
+        // Cursor
+        write_at(">", 1, 1, 3+cursorPos); 
+        for(int i=0;i<WIN_HEIGHT;i++){
+            write_at(artNames[i+winOffset], strlen(artNames[i+winOffset]), 2, 3+i); 
         }
-   
-        write_at("TE TOCA:", 8, 1, 1);
-        write_at(artNames[artIndex], strlen(artNames[artIndex]), 1, 2);
-        error_tone();
-        print_bitmapimage(hPrinter, 0, filenames[artIndex]);
-        print_bitmapimage(hPrinter, 0, padding);
-        SVC_WAIT(100);
 
-        clrscr();
-        write_at("Gracias!", 8, 1, 1);
-        write_at("Retira tu factura", 17, 1, 2);
-        SVC_WAIT(5000);
+        int pendingKeys = kbd_pending_count();
+        int down = 0;
+        int up = 0;
+        int print = 0;
+        if(pendingKeys){
+            char key[1];
+            read(console, key, 1);
+
+            down = key[0] == 0xE2;
+            up = key[0] == 0xE1;
+            print = key[0] == 0x8D;
+        }
+
+        if(down) {
+            if(cursorPos == WIN_HEIGHT-1){
+                if(winOffset < TOTAL_ELEMENTS-WIN_HEIGHT){
+                    winOffset ++;
+                }
+            } else {
+                cursorPos++;
+            }
+        } 
+
+        if(up){
+            if(cursorPos == 0){
+                if(winOffset > 0){
+                    winOffset --;
+                }
+            } else {
+                cursorPos--;
+            }
+        }
+
+        if(print){
+            int artIndex = winOffset+cursorPos;
+            if(artIndex == 0){
+                artIndex = rand()%TOTAL_LOGOS;
+            } else {
+                artIndex--;
+            }
+
+            write_at("TE TOCA:", 8, 1, 1);
+            write_at(artNames[artIndex], strlen(artNames[artIndex]), 1, 2);
+            error_tone();
+            print_bitmapimage(hPrinter, 0, filenames[artIndex]);
+            print_bitmapimage(hPrinter, 0, padding);
+            SVC_WAIT(100);
+
+            clrscr();
+            write_at("Gracias!", 8, 1, 1);
+            write_at("Retira tu factura", 17, 1, 2);
+            SVC_WAIT(5000);
+
+            normal_tone();
+        }
+        SVC_WAIT(1);
     }
 
     close(console);
